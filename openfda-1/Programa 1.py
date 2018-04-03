7ed9d4b73578624530aabde68b23a69afc004a88
asanro <anahsr1@gmail.com>
2018-04-03 16:13:41 +0200

    Practica 1 parte 1

--- openfda-1/Programa 1.py
+++ openfda-1/Programa 1.py
@@ -3,8 +3,8 @@ import json
 
 headers = {'User-Agent': 'http-client'}
 
-conn = http.client.HTTPSConnection("api.github.com")
-conn.request("GET", "/drug/event.json", None, headers)
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json", None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
@@ -12,5 +12,5 @@ conn.close()
 
 repos = json.loads(repos_raw)
 
-repo = repos[0]
-print("The owner of the first repository is", repo['owner']['login'])
+repo = repos['results'][0]
+print("The medication's id is ", repo['id'],", its purpose is ", repo['purpose'][0],", and the manufactured name is ", repo['manufactured_name'])
