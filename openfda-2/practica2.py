db2a0b8545bc0639afa2523357bf323949a2354c
asanro <37300217+asanro@users.noreply.github.com>
2018-04-05 00:00:13 +0200

    Update practica2.py

--- openfda-2/practica2.py
+++ openfda-2/practica2.py
@@ -2,16 +2,20 @@ import http.client
 import json
 
 headers = {'User-Agent': 'http-client'}
-
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json", None, headers)
+conn.request("GET", '/drug/label.json?&search=active_ingredient:"acetylsalicylic"&limit=100', None, headers)
+
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
+
 repos_raw = r1.read().decode("utf-8")
 conn.close()
 
 repos = json.loads(repos_raw)
-
-repo = repos['results'][0]
-
-print("The medication's id is ", repo['id'],", its purpose is ", repo['purpose'][0],", and the manufacturer name is ", repo['manufacturer_name'])
+repo = repos['results']
+for i in range(len(repo)):
+    print("Medication's id:", repo[i]['id'])
+    try:
+        print("-Manufacturer's name:", repo[i]['openfda']['manufacturer_name'][0])
+    except KeyError:
+        print("-Manufacturer's name not found")
